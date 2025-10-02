df9009f63538fcfd1a20c7132fe3cb110a0a66ba
migbyte-0 <152094060+migbyte-0@users.noreply.github.com>
2025-10-02 09:10:43 +0300

    Create init.lua

---
+++ lua/modelmage/init.lua
@@ -0,0 +1,28 @@
+---
+
+### Lua Code
+
+#### `lua/modelmage/init.lua`
+
+This is the main entry point that users will interact with.
+
+```lua
+local config = require('modelmage.config')
+local handlers = require('modelmage.handlers')
+
+local M = {}
+
+--- The main setup function for the plugin.
+-- Users will call this in their Neovim config.
+-- @param user_config table: A table of user-provided configuration options.
+function M.setup(user_config)
+  config.setup(user_config)
+end
+
+--- The primary function to start the model generation process.
+-- This is what the user command will call.
+function M.generate_model()
+  handlers.start_generation_flow()
+end
+
+return M
